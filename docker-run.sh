b21d049eb53b5efd2173195b50c42ea5d90b2b2a
Paul Lin <paul.lin@kkday.com>
2018-09-05 14:58:04 +0800

    增加 mobile-member-ci

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 81:80 -v $(pwd)/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt kkday-member-ci:latest
+docker run -d -p 81:80 -v ~/vbox/kkday-mobile-member-ci:/kkday-mobile-member-ci -v ~/vbox/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt kkday-member-ci:latest
