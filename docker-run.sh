06d25db638a99f8292919d619c86fde70b561532
Sean Hsu <sean.hsu@kkday.com>
2018-09-06 19:10:16 +0800

    add container name

--- docker-run.sh
+++ docker-run.sh
@@ -9,21 +9,21 @@ fi
 case "$1" in  
   
   'debug')
-    docker run -it --rm -p 8080:80 -v $(pwd)/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest /bin/bash
+    docker run -it --rm --name docker-member-ci -p 8080:80 -v $(pwd)/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest /bin/bash
     ;;
   
   'kkday-member-ci')
-    docker run -d -p 8080:80 -v $(pwd)/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest
+    docker run -d -p 8080:80 --name docker-member-ci -v $(pwd)/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest
     ;;
   
   'kkday-mobile-member-ci')
-    docker run -d -p 8081:80 -v $(pwd)/kkday-mobile-member-ci:/kkday-mobile-member-ci -v $(pwd)/nginx/mobile:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest
+    docker run -d -p 8081:80 --name docker-mobile-member-ci -v $(pwd)/kkday-mobile-member-ci:/kkday-mobile-member-ci -v $(pwd)/nginx/mobile:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest
     ;;
   
   'all')
-    docker run -d -p 8080:80 -v $(pwd)/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest
+    docker run -d --name docker-member-ci -p 8080:80 -v $(pwd)/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest
 
-    docker run -d -p 8081:80 -v $(pwd)/kkday-mobile-member-ci:/kkday-mobile-member-ci -v $(pwd)/nginx/mobile:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest
+    docker run -d --name docker-mobile-member-ci -p 8081:80 -v $(pwd)/kkday-mobile-member-ci:/kkday-mobile-member-ci -v $(pwd)/nginx/mobile:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt -v $(pwd)/tools:/tools kkday-ci:latest
     ;;
 esac
 
