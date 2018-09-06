67766ae4bacb01678ff83ed202acfaa8b5632e3f
Sean Hsu <sean.hsu@kkday.com>
2018-09-06 10:55:59 +0800

    update nginx config

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -d -p 81:80 -v ~/vbox/kkday-mobile-member-ci:/kkday-mobile-member-ci -v ~/vbox/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt kkday-member-ci:latest
+docker run -d -p 8080:80 -v $(pwd)/kkday-member-ci:/kkday-member-ci -v $(pwd)/nginx/www:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt kkday-member-ci:latest
+
+docker run -d -p 8081:80 -v $(pwd)/kkday-mobile-member-ci:/kkday-mobile-member-ci -v $(pwd)/nginx/mobile:/etc/nginx/sites-enabled/default -v $(pwd)/certificate/KKday_Root_CA.crt:/usr/local/share/ca-certificates/KKday_Root_CA.crt kkday-member-ci:latest
